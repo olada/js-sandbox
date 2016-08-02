94cc32a09b1ccaca0c6a4662574293d01195f43e
David Olah <david.olah@gmail.com>
2016-08-02 09:57:53 +0200

    added jshint options

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,41 @@ module.exports = {
 	},
 	resolve: {
 		root: path.resolve("./src/js")
+	},
+
+	// JSHint enforcing options
+	// http://jshint.com/docs/options/
+	jshint: {
+		// This option requires you to always put curly braces around 
+		// blocks in loops and conditionals.
+		curly: true,
+
+		// This options prohibits the use of == and != in favor of === and !==. 
+		// The former try to coerce values before comparing them which can 
+		// lead to some unexpected results. 
+		// The latter don't do any coercion so they are generally safer. 
+		eqeqeq: true,
+
+		// This option enables warnings about the use of identifiers 
+		// which are defined in future versions of JavaScript. 
+		// Although overwriting them has no effect in contexts where 
+		// they are not implemented, this practice can cause issues 
+		// when migrating codebases to newer versions of the language.
+		futurehostile: true,
+
+		// This option prohibits the use of explicitly undeclared variables. 
+		// This option is very useful for spotting leaking and mistyped variables.
+		undef: true,
+
+		// This option warns when you define and never use your variables. 
+		// It is very useful for general code cleanup, 
+		// especially when used in addition to undef.
+		unused: true,
+
+		// This option defines globals that are usually used for 
+		// logging poor-man's debugging: console, alert, etc. 
+		// It is usually a good idea to not ship them in production because, 
+		// for example, console.log breaks in legacy versions of Internet Explorer.
+		devel: true
 	}
 }
