a90a6e7f10effdbb94b48c8bfcc3edde29d31ab9
Daniel Moritz <daniel.moritz@cofinpro.de>
2016-08-26 21:29:58 +0200

    fixed git stashing problem

--- src/js/react/constants.js
+++ src/js/react/constants.js
@@ -2,8 +2,5 @@ export const INPUT_MONAT_NETTO = 'INPUT_MONAT_NETTO';
 export const INPUT_MONAT_AUSGABEN = 'INPUT_MONAT_AUSGABEN';
 export const INPUT_FREI_VERFUEGBAR_PERCENT = 'INPUT_FREI_VERFUEGBAR_PERCENT';
 export const INPUT_FREI_VERFUEGBAR_ABS = 'INPUT_FREI_VERFUEGBAR_ABS';
-<<<<<<< Updated upstream
 export const INPUT_CHILDREN = 'INPUT_CHILDREN';
-=======
 export const MONTHS_IN_YEAR = 12;
->>>>>>> Stashed changes
